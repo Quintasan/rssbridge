2a9a3ee706be992f55dbd3f13f80d0730ee2b9de
Michał Zając <michal.zajac@gmail.com>
2022-01-30 17:35:20 +0100

    Initial commit

---
+++ lib/rssbridge/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Rssbridge
+  VERSION = "0.1.0"
+end
---
+++ spec/rssbridge_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rssbridge do
+  it "has a version number" do
+    expect(Rssbridge::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
