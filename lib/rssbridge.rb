059657a5e90a5c30e44326ecd0cf88e8896518dc
Michał Zając <michal.zajac@gmail.com>
2022-01-30 17:35:35 +0100

    Install Zeitwerk

--- lib/rssbridge.rb
+++ lib/rssbridge.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require_relative "rssbridge/version"
+require 'zeitwerk'
+loader = Zeitwerk::Loader.for_gem
+loader.setup
 
 module Rssbridge
-  class Error < StandardError; end
-  # Your code goes here...
 end
